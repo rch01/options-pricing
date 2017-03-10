636d64b0f70f7bec5b2a3dfa7f9b48701260aa1a
rch <jwlkr01@gmail.com>
2017-03-10 01:31:56 +0000

    Removed all print statements, refactored getOptionGreeks() into three separate methods, removed getOptionProperties(), refactored calculateD1D2() such that d1 and d2 are now final instance variables, initialised only once in the constructor, removed DecimalFormat object.

--- src/optionspricing/blackscholes/Option.java
+++ src/optionspricing/blackscholes/Option.java
@@ -1,9 +1,5 @@
 package optionspricing.blackscholes;
 
-import java.text.DecimalFormat;
-
-/** Try to create an immutable class as per the definition */
-
 public final class Option {
 	final private char optionType;
 	final private double priceUnderlying;
@@ -11,11 +7,8 @@ public final class Option {
 	final private double timeToExpiry;
 	final private double interestRate;
 	final private double volatility;
-	
-	final private DecimalFormat df = new DecimalFormat("#,###.####");
-
-	private double d1;
-	private double d2;
+	final private double d1;
+	final private double d2;
 
 	public Option(char oType, double u, double k, double t, double r, double v) {
 		this.optionType = oType;
@@ -24,25 +17,24 @@ public final class Option {
 		this.timeToExpiry = t;
 		this.interestRate = r;
 		this.volatility = v;
+		this.d1 = D1D2.d1(this.priceUnderlying, this.priceExercise, this.timeToExpiry, this.volatility);
+		this.d2 = D1D2.d2(this.priceUnderlying, this.priceExercise, this.timeToExpiry, this.volatility);
 	}
 
-	public void price() {
-		this.calculateD1D2();
-		final double price = (this.optionType == 'c') ? getOptionPriceCall() : getOptionPricePut();
-		System.out.println("\nOption price: " + ((price == -1.0) ? "cannot calculate option price." : df.format(price)));
+	public double price() {
+		return (this.optionType == 'c') ? optionPriceCall() : optionPricePut();
 	}
 	
-	public void getOptionGreeks() {
-		final double delta = Greeks.delta(this.optionType, this.interestRate, this.timeToExpiry, d1);
-		final double gamma = Greeks.gamma(this.priceUnderlying, this.interestRate, this.timeToExpiry, this.volatility, d1);
-		final double theta = Greeks.theta(this.optionType, this.priceUnderlying, this.priceExercise, this.interestRate, this.timeToExpiry, this.volatility, d1, d2);
-		System.out.println("Delta: " + df.format(delta));
-		System.out.println("Theta: " + df.format(theta) + " (per calendar day)");
-		System.out.println("Gamma: " + df.format(gamma));
+	public double optionDelta() {
+		return Greeks.delta(this.optionType, this.interestRate, this.timeToExpiry, d1);
 	}
 
-	public void getOptionProperties() {
-		System.out.println(this);
+	public double optionGamma() {
+		return Greeks.gamma(this.priceUnderlying, this.interestRate, this.timeToExpiry, this.volatility, d1);
+	}
+
+	public double optionTheta() {
+		return Greeks.theta(this.optionType, this.priceUnderlying, this.priceExercise, this.interestRate, this.timeToExpiry, this.volatility, d1, d2);
 	}
 
 	@Override
@@ -52,16 +44,11 @@ public final class Option {
 				+ ", volatility=" + volatility + "]";
 	}
 
-	private void calculateD1D2() {
-		d1 = D1D2.d1(this.priceUnderlying, this.priceExercise, this.timeToExpiry, this.volatility);
-		d2 = D1D2.d2(this.priceUnderlying, this.priceExercise, this.timeToExpiry, this.volatility);
-	}
-
-	private double getOptionPriceCall() {
+	private double optionPriceCall() {
 		return Black76.optionPriceCall(this.priceUnderlying, this.priceExercise, this.timeToExpiry, this.interestRate, this.d1, this.d2);
 	}
 
-	private double getOptionPricePut() {
+	private double optionPricePut() {
 		return Black76.optionPricePut(this.priceUnderlying, this.priceExercise, this.timeToExpiry, this.interestRate, this.d1, this.d2);
 	}
 }
